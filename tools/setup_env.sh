41bd16e7c10a72d4014eec29100ee2050686e51b
Viet-Trung Luu <viettrungluu@chromium.org>
2015-03-31 08:12:15 -0700

    Delete setup_env.sh.

--- tools/setup_env.sh
+++
@@ -1,8 +0,0 @@
-# Copyright 2015 The Chromium Authors. All rights reserved.
-# Use of this source code is governed by a BSD-style license that can be
-# found in the LICENSE file.
-
-# Adds our tools directory to the path. To be sourced, not executed.
-
-# We're in the tools directory.
-export PATH="$(dirname "$(readlink -f "$0")"):${PATH}"
